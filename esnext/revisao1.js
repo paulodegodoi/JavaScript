cb50c437f67f529abec48c3c73774015b755f58e
PauloGodoiS <paulogodoi.sp@gmail.com>
2022-01-05 19:27:04 -0300

    revisando

--- esnext/revisao1.js
+++ esnext/revisao1.js
@@ -8,3 +8,15 @@
 console.log(a)
 
 // Template String
+const produto = 'iPad'
+console.log(`${produto} é caro!`)
+
+// Destructuring
+const [l, e, ...tras] = "Cod3r"
+console.log(l, e, tras)
+
+const [x, , y] = [1, 2, 3]
+console.log(x, y)
+
+const { idade: i, nome } = { nome: 'Ana', idade: 19 }
+console.log(i, nome)
