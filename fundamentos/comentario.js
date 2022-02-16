d6b43f7c744e7ba9b3f1c61fd7c05f776755dd70
PauloGodoiS <paulogodoi.sp@gmail.com>
2022-02-16 19:03:14 -0300

    revisando o js

---
+++ fundamentos/comentario.js
@@ -0,0 +1,14 @@
+// Comentário de única linha
+console.log('Linha 1')
+
+/* 
+    Comentário de 
+    múltiplas linhas
+*/
+console.log('Linha 2')
+
+/* 
+    * Comentário de
+    * múltiplas linhas
+    */
+console.log('Linha 3')
---
+++ fundamentos/organizacao.js
@@ -0,0 +1,8 @@
+console.log("sentença de código")
+
+{
+    {
+        console.log('Bloco dentro de outro bloco de código')
+    }
+    console.log('Bloco de código')
+}
