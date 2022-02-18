4a180a6bd1121f998bd06d2308867b7c95681e4a
PauloGodoiS <paulogodoi.sp@gmail.com>
2022-02-18 16:38:32 -0300

    revisando js

--- fundamentos/nullUndefined.js
+++ fundamentos/nullUndefined.js
@@ -11,3 +11,12 @@ console.log(produto)
 
 produto.preco = 3.50
 console.log(produto)
+
+produto.preco = undefined // evitar atribuir undefined
+console.log(!!produto.preco)
+// delete produto.preco
+console.log(produto)
+
+produto.preco = null // sem preco
+console.log(!!produto.preco)
+console.log(produto)
--- fundamentos/template string.js
+++
@@ -1,6 +0,0 @@
-const nome = "Rebeca"
-const concatenacao = 'Olá ' + nome + '!'
-const template = `
-    Olá
-    ${nome}!`
-console.log(concatenacao, template)
