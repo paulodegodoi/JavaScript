a043895fa446efb06b459b65c2d1699f30995440
PauloGodoiS <paulogodoi.sp@gmail.com>
2022-02-21 20:34:11 -0300

    revisão js

--- fundamentos/funcao2.js
+++ fundamentos/funcao2.js
@@ -1,18 +1,18 @@
-// Armazenando uma função em uma variavel
+// Armazenando uma função em uma variável
 const imprimirSoma = function (a, b) {
     console.log(a + b)
 }
 
 imprimirSoma(2, 3)
 
-// Armazenando uma função arrow em uma variavel
+// Armazenando uma função arrow em uma variável
 const soma = (a, b) => {
-    return (a + b)
+    return a + b
 }
 
 console.log(soma(2, 3))
 
-// retorno implicito
+// retorno implícito
 const subtracao = (a, b) => (a - b)
 
 console.log(subtracao(2, 3))
